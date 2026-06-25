0322f1a105aa56d2be2e52f3afe85df07a10cad8
Andre Himmighofen <andre@ahimmighofen.de>
2026-06-25 14:58:11 +0200

    Close shared HTTP session on shutdown

--- src/utils/lifecycle.py
+++ src/utils/lifecycle.py
@@ -1,10 +1,11 @@
 import logging
+from utils.http_client import close_http_session
 
 logger = logging.getLogger(__name__)
 
 
 def shutdown_display_stack(refresh_task, display_manager):
-    """Stop background refresh work, then release display resources best-effort."""
+    """Stop background refresh work, then release app resources best-effort."""
     try:
         refresh_task.stop()
     except Exception:
@@ -14,3 +15,8 @@ def shutdown_display_stack(refresh_task, display_manager):
         display_manager.close()
     except Exception:
         logger.exception("Exception while closing display manager during shutdown")
+
+    try:
+        close_http_session()
+    except Exception:
+        logger.exception("Exception while closing HTTP session during shutdown")
